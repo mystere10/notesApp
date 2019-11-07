7d87bfcc981b40bd1de3dce9f088fd45d73d3eae
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2019-11-07 10:16:39 +0200

    deploying to heroku

---
+++ build/precache-manifest.e079e7a7569c34b9e2903649e0e3ab80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76dbf18e10668797b40c12caf566ede",
+    "url": "/mystere10/notesApp/index.html"
+  },
+  {
+    "revision": "82dbb8e271ae3f1b5ae9",
+    "url": "/mystere10/notesApp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "40e2ba011f92ec273118",
+    "url": "/mystere10/notesApp/static/js/2.c58a2ecc.chunk.js"
+  },
+  {
+    "revision": "82dbb8e271ae3f1b5ae9",
+    "url": "/mystere10/notesApp/static/js/main.8b181405.chunk.js"
+  },
+  {
+    "revision": "7a82660a0edce5b25b1f",
+    "url": "/mystere10/notesApp/static/js/runtime-main.ab4f300b.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mystere10/notesApp/precache-manifest.e079e7a7569c34b9e2903649e0e3ab80.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mystere10/notesApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
